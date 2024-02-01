4924ad5b5e2e743cf6843baa64c6e9b9ba5374c0
Enzo Faria <143216954+Enzofdl@users.noreply.github.com>
2024-02-01 11:48:16 -0300

    Add files via upload

---
+++ Pratica poo/src/Relógio/teste.java
@@ -0,0 +1,20 @@
+package Relógio;
+
+public class teste {
+
+	public static void main(String[] args) {
+		Tempo a = new Tempo(23, 59, 00);
+		String aux = a.retornaTempo();
+		System.out.print(aux);
+		a.decrementaM();
+		aux = a.retornaTempo();
+		System.out.print(aux);
+		a.incrementaM();
+		a.incrementaM();
+		aux = a.retornaTempo();
+		System.out.print(aux);
+		
+
+	}
+
+}
