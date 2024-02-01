580c0d098273111c7430a481994505e8e68dbaae
Enzo Faria <143216954+Enzofdl@users.noreply.github.com>
2024-02-01 11:57:31 -0300

    Add files via upload

---
+++ Construtores/src/Ponto/Pontoo.java
@@ -0,0 +1,20 @@
+package Ponto;
+
+public class Pontoo {
+	private double x, y;
+	
+	
+	
+	public double getX() {
+		return x;
+	}
+	public double getY() {
+		return y;
+	}
+	public Pontoo() {}
+	public Pontoo(double x, double y) {
+		this.x = x;
+		this.y = y;
+	}
+
+}
---
+++ Construtores/src/Ponto/Teste.java
@@ -0,0 +1,12 @@
+package Ponto;
+
+public class Teste {
+
+	public static void main(String[] args) {
+		Pontoo a = new Pontoo();
+		Pontoo b = new Pontoo(1, 2);
+		System.out.println(a.getX() + " " + a.getY());
+		System.out.println(b.getX() + " " + b.getY());
+	}
+
+}
